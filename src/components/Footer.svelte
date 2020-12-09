d08eb4aa837098480f741564e1cdebacc2752f33
Raymundo Solis <pepperray01@gmail.com>
2020-12-09 03:47:53 -0600

    First commit social pugs

---
+++ src/components/Footer.svelte
@@ -0,0 +1,16 @@
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
+
+<div class="Footer">
+  <div class="Footer-container">
+    <div class="Footer-copy">
+      © 2020 Pugstagram from platzi
+    </div>
+  </div>
+</div>
